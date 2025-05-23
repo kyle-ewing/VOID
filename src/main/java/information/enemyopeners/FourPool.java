fdee68196777464ba2c095f502cb77724dd08813
kyle-ewing <kylemewing@gmail.com>
2025-05-23 00:38:49 -0700

    added 4pool enemy strat

---
+++ src/main/java/information/enemyopeners/FourPool.java
@@ -0,0 +1,28 @@
+package information.enemyopeners;
+
+import bwapi.UnitType;
+import information.EnemyUnits;
+
+import java.util.HashSet;
+
+public class FourPool extends EnemyStrategy {
+    public FourPool() {
+        super("Four Pool");
+    }
+
+    public boolean isEnemyStrategy(HashSet<EnemyUnits> enemyUnits, int frameCount) {
+        for(EnemyUnits enemyUnit : enemyUnits) {
+            if(enemyUnit.getEnemyType() == UnitType.Zerg_Spawning_Pool) {
+                if(frameCount < 2400) {
+                    return true;
+                }
+            }
+            else if(enemyUnit.getEnemyType() == UnitType.Zerg_Zergling) {
+                if(frameCount < 4200) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
