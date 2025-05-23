12d5db76045f4ab8e0c2791d61c81126fecaecf0
kyle-ewing <kylemewing@gmail.com>
2025-05-23 00:02:47 -0700

    added enemystrategy class

---
+++ src/main/java/information/enemyopeners/EnemyStrategy.java
@@ -0,0 +1,15 @@
+package information.enemyopeners;
+
+import information.EnemyUnits;
+
+import java.util.HashSet;
+
+public abstract class EnemyStrategy {
+    private String strategyName;
+
+    public EnemyStrategy(String strategyName) {
+        this.strategyName = strategyName;
+    }
+
+    public abstract boolean isEnemyStrategy(HashSet<EnemyUnits> enemyUnits, int frameCount);
+}
