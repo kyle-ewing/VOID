4bcb71d3a8ae0e1c7bbf5be5375ddb64f4f84889
Kyle Ewing <47468100+kyle-ewing@users.noreply.github.com>
2025-06-12 15:11:40 -0700

    refactor: added ebay and 1/1 to tworaxacademy, adjusted moveout (#46)
    Added ebay and 1/1 to TwoRaxAcademy
    
    Added upgrade level to PlannedItem
    
    Adjusted Build Orders to handle PI constructor changes
    
    Increased required marines for TRA pushout

--- src/main/java/macro/buildorders/TwoRaxAcademy.java
+++ src/main/java/macro/buildorders/TwoRaxAcademy.java
@@ -25,8 +25,11 @@ public class TwoRaxAcademy implements BuildOrder {
         buildOrder.add(new PlannedItem(UnitType.Terran_Academy, 19, PlannedItemStatus.NOT_STARTED, PlannedItemType.BUILDING, 2));
         buildOrder.add(new PlannedItem(UnitType.Terran_Supply_Depot, 24, PlannedItemStatus.NOT_STARTED, PlannedItemType.BUILDING, 2));
         buildOrder.add(new PlannedItem(TechType.Stim_Packs, 26, PlannedItemStatus.NOT_STARTED, PlannedItemType.UPGRADE, UnitType.Terran_Academy, 2));
-        buildOrder.add(new PlannedItem(UpgradeType.U_238_Shells, 26, PlannedItemStatus.NOT_STARTED, PlannedItemType.UPGRADE, UnitType.Terran_Academy, 2));
+        buildOrder.add(new PlannedItem(UpgradeType.U_238_Shells, 26, PlannedItemStatus.NOT_STARTED, PlannedItemType.UPGRADE, UnitType.Terran_Academy, 1, 2));
+        buildOrder.add(new PlannedItem(UnitType.Terran_Engineering_Bay, 24, PlannedItemStatus.NOT_STARTED, PlannedItemType.BUILDING, 2));
         buildOrder.add(new PlannedItem(UnitType.Terran_Comsat_Station, 28, PlannedItemStatus.NOT_STARTED, PlannedItemType.ADDON, 2));
+        buildOrder.add(new PlannedItem(UpgradeType.Terran_Infantry_Weapons, 28, PlannedItemStatus.NOT_STARTED, PlannedItemType.UPGRADE, UnitType.Terran_Engineering_Bay, 1, 2));
+        buildOrder.add(new PlannedItem(UpgradeType.Terran_Infantry_Armor, 28, PlannedItemStatus.NOT_STARTED, PlannedItemType.UPGRADE, UnitType.Terran_Engineering_Bay, 1, 3));
         return buildOrder;
     }
 }
