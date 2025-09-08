64b88d7926e1fec54a5108ede62939b6dc6a3f73
kyle-ewing <kylemewing@gmail.com>
2025-09-07 22:21:22 -0700

    refactor: updated rallypoints to account for pathfinding

---
+++ src/main/java/macro/RallyPoint.java
@@ -0,0 +1,70 @@
+package macro;
+
+import bwapi.Position;
+import bwem.Base;
+import information.BaseInfo;
+import information.EnemyInformation;
+import information.enemyopeners.EnemyStrategy;
+import macro.unitgroups.CombatUnits;
+import map.PathFinding;
+import util.PositionInterpolator;
+import java.util.List;
+
+public class RallyPoint {
+    private PathFinding pathFinding;
+    private EnemyInformation enemyInformation;
+    private BaseInfo baseInfo;
+    private EnemyStrategy enemyStrategy = null;
+    private Base startingBase;
+
+    public RallyPoint(PathFinding pathFinding, EnemyInformation enemyInformation, BaseInfo baseInfo) {
+        this.pathFinding = pathFinding;
+        this.enemyInformation = enemyInformation;
+        this.baseInfo = baseInfo;
+
+        this.startingBase = baseInfo.getStartingBase();
+
+    }
+
+    public void setRallyPoint(CombatUnits combatUnit) {
+        if(enemyInformation.getEnemyOpener() == null || enemyStrategy.isStrategyDefended()) {
+            if(baseInfo.getOwnedBases().contains(baseInfo.getNaturalBase())) {
+                combatUnit.setRallyPoint(rallyPath(startingBase.getCenter(), baseInfo.getNaturalChoke().getCenter().toPosition()).toTilePosition());
+            }
+            else {
+                System.out.println(startingBase.getCenter() + " : " + baseInfo.getMainChoke().getCenter().toPosition());
+
+                combatUnit.setRallyPoint(rallyPath(startingBase.getCenter(), baseInfo.getMainChoke().getCenter().toPosition()).toTilePosition());
+            }
+        }
+        else if(enemyInformation.getEnemyOpener().getStrategyName().equals("Four Pool")) {
+            combatUnit.setRallyPoint(baseInfo.getStartingBase().getCenter().toTilePosition());
+        }
+    }
+
+    private Position rallyPath(Position startingPos, Position endPoint) {
+        Position nearestWalkable = pathFinding.findNearestWalkable(startingPos);
+        Position nearestWalkableToEnd = pathFinding.findNearestWalkable(endPoint);
+        List<Position> path;
+
+        if(nearestWalkable != null && nearestWalkableToEnd != null) {
+            path = pathFinding.findPath(nearestWalkable, nearestWalkableToEnd);
+        }
+        else {
+            path = pathFinding.findPath(startingPos, endPoint);
+        }
+
+
+        if(path == null || path.isEmpty()) {
+            return endPoint;
+        }
+
+        return PositionInterpolator.interpolate(path, 0.75);
+    }
+
+    public void onFrame() {
+        enemyStrategy = enemyInformation.getEnemyOpener();
+    }
+
+
+}
